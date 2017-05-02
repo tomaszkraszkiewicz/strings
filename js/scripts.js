76e9f82351977eca64c3ca0e3fba923e32869743
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-05-02 11:40:37 +0200

    script updatede

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach."
-var animal = "Zielone słonie"
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var animal = "Zielone słonie";
+
+var animalUpper = animal.toUpperCase();
+
+console.log(animalUpper);
+
+
+var greenElephants = text.replace('Papugi', animalUpper);
+
+console.log(greenElephants);
+console.log((greenElephants.length)/2);
